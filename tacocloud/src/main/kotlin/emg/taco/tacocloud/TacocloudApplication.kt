b31fee8b69bcb2b968773b83914018c40a9478db
petrubear <edison.martinez@gmail.com>
2022-08-06 17:37:23 -0500

    chore: remover clases que ya no se usan

--- tacocloud/src/main/kotlin/emg/taco/tacocloud/TacocloudApplication.kt
+++ tacocloud/src/main/kotlin/emg/taco/tacocloud/TacocloudApplication.kt
@@ -6,13 +6,12 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EntityScan("emg.taco.*")
 @EnableJpaRepositories("emg.taco.*")
-class TacocloudApplication (val repo: IngredientRepository) : CommandLineRunner {
+class TacocloudApplication(val repo: IngredientRepository) : CommandLineRunner {
     override fun run(vararg args: String?) {
         repo.save(Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP))
         repo.save(Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP))
