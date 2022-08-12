30ccb8e8e67d2154dae862cbc5d0dfb873292d39
petrubear <edison.martinez@gmail.com>
2022-08-12 16:38:18 -0500

    feat: ch6 profiles

--- tacocloud/src/main/kotlin/emg/taco/tacocloud/TacocloudApplication.kt
+++ tacocloud/src/main/kotlin/emg/taco/tacocloud/TacocloudApplication.kt
@@ -2,6 +2,7 @@ package emg.taco.tacocloud
 
 import emg.taco.tacocloud.data.repository.IngredientRepository
 import emg.taco.tacocloud.model.Ingredient
+import mu.KotlinLogging
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
@@ -9,12 +10,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+private val log = KotlinLogging.logger {}
+
 @SpringBootApplication
 @EntityScan("emg.taco.*")
 @EnableJpaRepositories("emg.taco.*")
 @EnableConfigurationProperties
 class TacocloudApplication(val repo: IngredientRepository) : CommandLineRunner {
     override fun run(vararg args: String?) {
+        log.debug { "Setting up ingredients" }
         repo.save(Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP))
         repo.save(Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP))
         repo.save(Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN))
@@ -25,6 +29,7 @@ class TacocloudApplication(val repo: IngredientRepository) : CommandLineRunner {
         repo.save(Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE))
         repo.save(Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE))
         repo.save(Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE))
+        log.debug { "Ingredientes repo count: ${repo.count()}" }
     }
 
 }
